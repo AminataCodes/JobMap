e50c11f4f090a8e0e1b98bc74c40c931a2c12b47
Aminata Konate <Aminataingreen@gmail.com>
2026-06-18 21:50:55 +0200

    prisma js change back to normal

--- Back-end/src/lib/prisma.js
+++ Back-end/src/lib/prisma.js
@@ -1,16 +1,9 @@
 import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
 
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false  // ← fix pour Render
-  }
-})
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
-const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
-export default prisma
+export default prisma 
